b785fe3c662b92a2ba23be2569721d1847ec925b
AlanAssembly <47705380+AlanAssembly@users.noreply.github.com>
2019-02-16 23:59:20 -0300

    Add files via upload

---
+++ CheckGround.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=GetComponentInParent<PlayerController>();
+       
+    }
+
+    // Update is called once per frame
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag=="Ground") {
+
+            player.grounded = true;
+
+        }
+        
+
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Ground")
+        {
+
+
+            player.grounded = false;
+        }
+
+    }
+}
---
+++ EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float Speed=1f;
+    public float maxSpeed=1f;
+
+    private Rigidbody2D rgbd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rgbd.AddForce(Vector2.right * Speed);
+        float limitedspeed = Mathf.Clamp(rgbd.velocity.x, -maxSpeed, maxSpeed);
+        rgbd.velocity = new Vector2(limitedspeed, rgbd.velocity.y);
+    }
+}
